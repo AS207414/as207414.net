b68b5ec5dee84facde5ffa2a7c074ca845ab40fb
Ganawa Juanah <ganawa@juanah.com>
2022-03-24 08:12:28 -0500

    add addr flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
